adf57f94bef6b1998f7e58bfdc9c8e6f780c9e3d
MrRamis <99258165+MrRamis@users.noreply.github.com>
2022-12-04 21:11:02 +0300

    Revert "Test3"
    This reverts commit 1a6d7e382ed94bb0e2aab262b98f529fc88079bd.

--- Source/No_d_3rdP/No_d_3rdP.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "No_d_3rdP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, No_d_3rdP, "No_d_3rdP" );
